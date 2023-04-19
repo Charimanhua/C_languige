557daa9590eabcbc9f316aacfc568b836941936a
Charimanhua <115345164+Charimanhua@users.noreply.github.com>
2023-04-19 19:57:24 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include"func.h"
 
-//¿ªÊ¼Ê±¼ä:04-19--13:46
+//Â¿ÂªÃŠÂ¼ÃŠÂ±Â¼Ã¤:04-19--13:46
 int main() {
 	Pm a = Matcreate_1();
-	printf("Êä³öÈçÏÂ:>\n");
+	printf("ÃŠÃ¤Â³Ã¶ÃˆÃ§ÃÃ‚:>\n");
 	for (int i = 0; i < a->row; i++) {
 		for (int j = 0; j < a->row; j++) {
 			printf("%lf ", a->cont[i][j]);
 		}
 		printf("\n");
 	}
+	printf("ahhaha");
 	return 0;
 } 
